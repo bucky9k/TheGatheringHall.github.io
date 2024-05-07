6848d382ba9d5b4751deb136cd66985824841033
bucky9k <bucky9k@gmail.com>
2024-05-06 23:53:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// Add event listeners to each checkbox
 document.querySelectorAll('.item input[type="checkbox"]').forEach(function(checkbox) {
     checkbox.addEventListener('change', function() {
         // Toggle the "checked" class on the parent item div
         this.parentNode.classList.toggle('checked');
+        
+        // Update the checkbox's appearance
+        if (this.checked) {
+            this.setAttribute('checked', 'checked');
+        } else {
+            this.removeAttribute('checked');
+        }
 
         // Add additional actions as needed
     });
